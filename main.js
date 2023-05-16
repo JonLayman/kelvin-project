8e3fc7c453bf0e4a267e3347ad08eaf6afaa23c4
JonLayman <133721255+JonLayman@users.noreply.github.com>
2023-05-15 20:21:31 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,33 @@
+//Forecast for the day in Kelvin
+const kelvin = 293;
+
+console.log(kelvin);
+
+//Converting Kelvin to Celsius
+
+var difference = 293 - 273;
+
+console.log(difference);
+
+//Conversion of Kelvin to Celsius
+const Celsius = 20;
+
+console.log(Celsius);
+
+ //Converstion from Celsius to Fahrenheit
+var Fahrenheit = Celsius * (9/5) + 32;
+
+
+console.log(Fahrenheit);
+
+//Assigning Fahrenheit to a Variable
+var Fahrenheit = 68;
+
+var TEMPERATURE = 68;
+
+console.log(`The temperature is ${TEMPERATURE} degrees in farenheit`);
+
+
+
+
+
